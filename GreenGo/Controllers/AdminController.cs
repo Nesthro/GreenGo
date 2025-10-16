010abb39b9b441a021c4c2791724522b0556824a
Jankyyyy <paroni.johnkyle.senecio@gmail.com>
2025-10-16 11:56:15 +0800

    Add files via upload

---
+++ GreenGo/Controllers/AdminController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GreenGo.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Admin/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+        // GET: Admin/Destinations
+        public ActionResult Destinations()
+        {
+            return View();
+        }
+        // GET: Admin/Users
+        public ActionResult Users()
+        {
+            return View();
+        }
+        // GET: Admin/Transactions
+        public ActionResult Transactions()
+        {
+            return View();
+        }
+        // GET: Admin/Reservations
+        public ActionResult Reservations()
+        {
+            return View();
+        }
+        // GET: Admin/Categories
+        public ActionResult Categories()
+        {
+            return View();
+        }
+    }
+}
